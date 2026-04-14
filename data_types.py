6fd8259f204165b2ab0e9dc2fbad37f66fdb6098
momina <momina.ausaf.92@gmail.com>
2026-04-14 19:18:54 +0100

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,15 @@
+print("welcome to my projects")
+
+variable = input("what are you learning")
+variable2 = input("what are data types")
+
+print("you are learning "+variable+" "+variable2)
+print("well done")
+
+print("now tell me the data types of following")
+
+name = input("whats your name?")
+age = int(input("whats your age"))
+
+print(type(name))
+print(type(age))
